af1e48f915d389e4d0a175707d5c4bf48bd17e5f
BetoNajera <betomin.unica@gmail.com>
2019-11-21 00:09:04 -0600

    Se arreglaron errores

--- src/macroe/MacroE.java
+++ src/macroe/MacroE.java
@@ -17,20 +17,14 @@ public class MacroE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
         Z80 x = new Z80();
         x.read("programa");
-||||||| a474dcb
-        // TODO code application logic here
-=======
+        
+        
         String bin = "01rs";
         ArrayList <String> variables = new ArrayList <> ();
         variables.add("111");
         variables.add("00000100");
-        
-        
-        
->>>>>>> 58931e294c5507debd62784bdae1f9ac95bf8128
     }
     
 }
